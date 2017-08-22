26ff0ebedd722e3850e9b443273043ea89c8ffd9
Peter Johnson <pjohnson141@gmail.com>
2017-08-22 16:05:38 +0000

    Added students

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :students
   root 'static_pages#home'
   get 'static_pages/home'
   get 'random/core1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+c1 = [ 
+  
+  "Alberts, Makenzie G. ",
+"Berger, Gene R. ",
+"Block, Dalton M. ",
+"Closner, Blake A. ",
+"Collins-Simones, Anthony G. ",
+"Ellis, Ashlyn M. ",
+"Hodge, Faith M. ",
+"Holzer, Krista J. ",
+"Jiorle, Xavier D. ",
+"Johnston, Anastasia M. ",
+"Keigley, Karlie D. ",
+"Klingsporn, Adam J. ",
+"Kubista, Dylan S. ",
+"Kuehl, Riley C. ",
+"Kundert, Kara M. ",
+"Larson, Anders A. ",
+"McClure, Kendra R. ",
+"Noll, Jack B. ",
+"Northrop, Gabriel J. ",
+"Olson, Samuel D. ",
+"Ramirez, Isaiah ",
+"Rogers, Andrew J. ",
+"Schaefer, Braden S. ",
+"Simon, Caitlyn J. ",
+"Strop, Addison R. ",
+"Wangsness, Lauren E. ",
+"Williams, Cooper M. "
+
+    ]
+    
+c1.each do |name|
+    Student.create!(name:  name,
+             hour: 1,
+             call_count: 0)
+end 
